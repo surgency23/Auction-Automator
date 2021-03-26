38bcd4a5b3f4c25cb08d8865d3af0f258f5ed7f1
Alex Pappa <monkiebrain@MonkieBrainsAir.wowway.com>
2021-03-25 23:58:06 -0400

    building project

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.IO;
+
+namespace Test {
+    class Program {
+        static void Main(string[] args) {
+            string html = string.Empty;
+            string url = @"https://www.capitalcityonlineauction.com/cgi-bin/mncal.cgi?ccoa";
+
+            HttpWebRequest request = (HttpWebRequest) WebRequest.Create(url);
+            request.AutomaticDecompression = DecompressionMethods.GZip;
+
+            using(HttpWebResponse response = (HttpWebResponse) request.GetResponse())
+            using(Stream stream = response.GetResponseStream())
+            using(StreamReader reader = new StreamReader(stream)) {
+
+                html = reader.ReadToEnd();
+            }
+
+            Console.WriteLine(html);
+        }
+    }
+}
